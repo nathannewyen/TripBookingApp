d5364cdffa92947ebbafd831b17937d8741996e9
Nhan Nguyen <nathan13574@gmail.com>
2020-11-25 19:12:59 -0600

    update

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Image, TouchableOpacity} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {NavigationContainer, DefaultTheme} from '@react-navigation/stack';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 
 // Screen
-import {OnBoarding, DestinationDetails} from './screens';
+import {OnBoarding} from './screens/';
+import {COLORS, icons, SIZES} from './constants';
+
+// Tabs
+import Tabs from './navigation/tabs';
 
 const theme = {
   ...DefaultTheme,
@@ -21,7 +25,74 @@ const App = () => {
     <NavigationContainer theme={theme}>
       <Stack.Navigator initialRouteName={'OnBoarding'}>
         {/* Screens */}
-        <Stack.Screen name="OnBoarding" component={OnBoarding} />
+        <Stack.Screen
+          name="Onboarding"
+          component={OnBoarding}
+          options={{
+            title: null,
+            headerStyle: {
+              backgroundColor: COLORS.white,
+            },
+            headerLeft: null,
+            headerRight: () => (
+              <TouchableOpacity
+                style={{marginRight: SIZES.padding}}
+                onPress={() => console.log('Pressed')}>
+                <Image
+                  source={icons.barMenu}
+                  resizeMode="contain"
+                  // eslint-disable-next-line react-native/no-inline-styles
+                  style={{
+                    width: 25,
+                    height: 25,
+                  }}
+                />
+              </TouchableOpacity>
+            ),
+          }}
+        />
+
+        {/* Tabs */}
+        <Stack.Screen name="Home" component={Tabs} options={{
+          title: null,
+          headerStyle: {
+            backgroundColor: COLORS.white
+          },
+          headerLeft: ({onPress}) => (
+            <TouchableOpacity
+              style={{ marginLeft: SIZES.padding }}
+              onPress={onPress}
+            >
+            <Image
+              source={icons.back}
+              resizeMode='contain'
+              style={{
+                width: 25,
+                height: 25,
+              }}
+            />    
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity
+              style={{ marginRight: SIZES.padding }}
+              onPress={() => console.log("Menu")}
+            >
+              <Image
+                source={icons .menu}
+                resizeMode='contain'
+                style={{
+                  width: 25,
+                  height: 25,
+                }}
+              />
+            
+            </TouchableOpacity>
+          )
+
+
+          }} 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
